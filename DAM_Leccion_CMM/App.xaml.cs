94e051c9a7fdb4e2ca0e251e63184b28b174068b
MERCK-X <21030463@itesa.edu.mx>
2025-01-28 17:48:32 -0600

    Version_01: Se creó el proyecto MAUI vacio.

---
+++ DAM_Leccion_CMM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DAM_Leccion_CMM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
