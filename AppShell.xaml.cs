24eb7540350b43dc292b7dc5cf4f62682b17aacf
MERCK-X <21030463@itesa.edu.mx>
2025-01-28 18:52:33 -0600

    Version_02: Instrucciones basicas en boton y caja de texto

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DAM_Leccion_0
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DAM_Leccion_0.Model;
+
+namespace DAM_Leccion_0
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Ejecutar();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar()
+        {
+            PersonaModel personaModel = new PersonaModel();
+            personaModel.Nombre = "Hola, aquitoi";
+            //txtNombre.Text = "Hola, aquitoi";
+        }
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema", "Se ha guardado el registro en la BD","Aceptar");
+        }
+    }
+
+}
