ec49c816d816a179be7e70802208f7d9f7b471ff
MERCK <21030463@itesa.edu.mx>
2025-02-03 17:41:20 -0600

    Version_04: Base ejemplo de creacion del proyecto

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,14 +14,7 @@ namespace DAM_Leccion_0
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            
         }
 
         public void Ejecutar()
